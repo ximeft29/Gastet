81147e0667e771fc6652faf882d2cd634b46ce5b
Ximena Flores de la Tijera <ximenafloresdelatijera@ximenas-macbook-pro.local>
2018-05-23 16:36:20 -0500

    Code Corrections - Ad post - Change Button Images :D

--- Gastet/PostViewController.swift
+++ Gastet/PostViewController.swift
@@ -8,20 +8,44 @@
 
 import UIKit
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
+    @IBOutlet weak var imagePosted: UIImageView!
+    
+    @IBAction func choseImageButton(_ sender: UIButton) {
+    
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        self.present(imagePicker, animated: true, completion: nil)
+    
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imagePosted.image = image
+            
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
     @IBOutlet weak var changeImageLostButton: UIButton!
     
     @IBAction func lostPressedButton(_ sender: UIButton) {
     
-        changeImageLostButton.setImage(UIImage(named:"LostButton-active.png"), for: .normal)
+    changeImageLostButton.setImage(UIImage(named:"LostButton-active.png"), for: .normal)
     }
     
     @IBOutlet weak var changeImageFoundButton: UIButton!
     
-    @IBAction func foundPressedButton(_ sender: UIButton) {
+    @IBAction func foundPressedButton(_ sender: Any) {
     
-          changeImageFoundButton.setImage(UIImage(named:"FoundButton-active.png"), for: .normal)
+        changeImageFoundButton.setImage(UIImage(named:"FoundButton-active.png"), for: .normal)
+        
     }
     
     override func viewDidLoad() {
